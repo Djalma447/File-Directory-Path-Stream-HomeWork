86df84b7ad16e5bb9948f2f1935eff0cc17685b5
Djalma <djalma.ant@gmail.com>
2020-03-31 19:09:04 -0300

    HomeWork File Directory Path Stream IO

---
+++ Course/Entities/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ","
+                + Total().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Globalization;
+using Course.Entities;
 
 namespace Course
 {
@@ -6,7 +9,38 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the path of your file: ");
+            string sourcePath = Console.ReadLine();
+            Item product;
+
+            try
+            {
+                string[] list = File.ReadAllLines(sourcePath);
+
+                string sourceFolder = Path.GetDirectoryName(sourcePath);
+                string targetFolder = sourceFolder + @"\out";
+                string targetFile = targetFolder + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolder);
+
+                using (StreamWriter sw = File.AppendText(targetFile))
+                {
+                    foreach (string x in list)
+                    {
+                        string[] var = x.Split(';');
+                        string name = var[0];
+                        double price = double.Parse(var[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(var[2]);
+                        product = new Item(name, price, quantity);
+                        sw.WriteLine(product);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Program Error");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
